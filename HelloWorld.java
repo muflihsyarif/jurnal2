c9d5372db043c0d76e3fede9e8ebb9e872d441db
unknown <muflihsyarif44@gmail.com>
2023-09-27 13:34:26 +0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main (String[] args) {
+        System.out.println("Nama : muhammad muflih syarif");
+        System.out.println("Nim : 607062300023");
+        System.out.println("kelas: D3IF-47-03");
+
+    }
+}
